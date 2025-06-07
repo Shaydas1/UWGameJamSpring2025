c03c9161d77a62ed2e01a439441490db15e5ee77
Lex Stapleton <lex.stapleton1423@gmail.com>
2025-06-07 11:03:29 -0400

    made the controller work nicely

--- uwgamejamspring2025/Scripts/Player/hook.gd
+++ uwgamejamspring2025/Scripts/Player/hook.gd
@@ -1,74 +1,164 @@
 extends RigidBody2D
 class_name Hook
 
-@export var water_gravity_scale : float = 0.1
-@export var water_damping : float = 0
+@export var water_gravity_scale : float = 0.8
+@export var water_damping : float = 0.3
 
-@export var max_speed : float = 100000
+@export var max_fall_speed : float = 500
 
+@export var max_distance : float
+@export var jerk_cut_factor: float = 0.2
 signal entered_water
 signal exited_water
 
 signal hook_lost
 
-var in_water := false
-var paused := false 
+var in_water : bool = false
+
+var reeling : bool = false
+var reel_strength : float = false
+
+var enabled : bool = false 
 
 
-func pause(value: bool):
-	set_deferred("paused", value)
-	set_deferred("sleeping", value)
-	set_deferred("freeze", value)
+var target : Node2D = null
 
 
-func on_enter_water():
-	if paused:
+func enable():
+	enabled = true
+	in_water = false
+	reeling = false
+	set_gravity()
+
+
+func disable():
+	enabled = false
+
+
+func cast(strength: float, angle : float):
+	if in_water:
 		return
-	print_debug("enter water")
+	
+	var direction = Vector2.UP.rotated(deg_to_rad(angle))
+	call_deferred("apply_central_impulse", direction * strength)
+	
+
+func _set_reeling(value: bool):
+	if not in_water:
+		return
+	
+	reeling = value
+	set_gravity()
+
+
+func start_reeling(strength: float):
+	_set_reeling(true)
+	reel_strength = strength
+	print_debug(gravity_scale, in_water)
+
+
+func stop_reeling():
+	_set_reeling(false)
+	print_debug(gravity_scale, in_water)
+	
+
+func _get_direction_to_target() -> Vector2:
+	return (target.global_position - global_position).normalized()
+
+
+func jerk(strength: float):
+	if not in_water:
+		return _get_direction_to_target
+	
+	linear_velocity = Vector2.ZERO
+	call_deferred("apply_central_impulse", strength * _get_direction_to_target())
+
+
+func end_jerk():
+	if linear_velocity.y < 0:
+		linear_velocity.y *= jerk_cut_factor
+	
+	if linear_velocity.x < 0:
+		linear_velocity.x *= jerk_cut_factor
+
+func reel():
+	apply_central_force(reel_strength * _get_direction_to_target())
+
+func initialize(target: Node2D):
+	self.target = target
+
+
+func set_gravity():
+	if reeling:
+		gravity_scale = 0
+		return
 		
-	gravity_scale = water_gravity_scale
-	linear_damp = water_damping
+	if in_water:
+		gravity_scale = water_gravity_scale
+	
+	else:
+		gravity_scale = 1
+
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if not enabled: 
+		state.linear_velocity = Vector2.ZERO
+		
+	if not in_water:
+		return
+	
+	state.linear_velocity.x = move_toward(state.linear_velocity.x, 0.0, 0.1 * state.step)
+		
+
+func _physics_process(delta):
+	if not enabled:
+		return
+	
+	
+		
+	if position.x < 0:
+		position.x = 0
+		
+	if in_water:
+		if reeling: 
+			reel()
+		if linear_velocity.y > max_fall_speed:
+			linear_velocity.y = max_fall_speed
+		
+	var offset = global_position - target.global_position
+	
+	if offset.length() > max_distance and linear_velocity.dot(offset) > 0:
+		apply_central_impulse(-linear_velocity.project(offset.normalized())*1.5)
+		
+func on_enter_water():
 	in_water = true
+	set_gravity()
+	linear_velocity.x = 0
+	linear_damp = water_damping
 	emit_signal("entered_water")
 
 
 func on_exit_water():
-	if paused:
-		return
-	print_debug("exit water")
-		
-	gravity_scale = 1.0
-	linear_damp = 0.1
 	in_water = false
+	set_gravity()
+	linear_damp = 0.1
 	emit_signal("exited_water")
 
 
 func _on_area_entered(area):
-
+	if not enabled:
+		return
+		
 	if area.is_in_group("water"):
 		on_enter_water()
 	
-	if paused:
-		return
 		
 	if area.is_in_group("rock"):
 		emit_signal("hook_lost")
 
 func _on_area_exit(area):
+	if not enabled:
+		return
+		
 	if area.is_in_group("water"):
 		on_exit_water()
-
-
-func _physics_process(delta):		
-	if in_water: 
-		if position.x < 0:
-			position.x = 0
-		
-		var vel = linear_velocity
-		
-		vel.y = clamp(vel.y, -INF, 100)
-		
-		linear_velocity.clamp()
-		if linear_velocity.length() > max_speed:
-			linear_velocity = linear_velocity.normalized() * max_speed
-		
--- uwgamejamspring2025/Scripts/Player/player.gd
+++ uwgamejamspring2025/Scripts/Player/player.gd
@@ -1,11 +1,8 @@
 extends Node2D
 class_name Player
 
-@export var cast_strength: float = 1000.0
+@export var cast_strength: float = 5000.0
 @export var cast_angle: float = 45
-
-@export var reel_strength: float = 1000
-@export var jerk_strength: float = 1000
 
 @onready var hook : Hook
 
@@ -14,10 +11,13 @@ var hook_held : bool
 
 func _ready():
 	hook = get_node("Hook")
-	take_hook()
+	on_hook_exit_water()
 	
-	hook.exited_water.connect(self.take_hook)
+	hook.initialize(self)
+	$JerkController.initialize(hook)
 	
+	hook.exited_water.connect(self.on_hook_exit_water)
+	hook.entered_water.connect(self.on_hook_enter_water)
 
 
 func _process(delta):
@@ -25,27 +25,21 @@ func _process(delta):
 		if Input.is_action_just_pressed("ui_accept"):
 			cast_hook()
 		
-	else:
-		if Input.is_action_just_pressed("ui_accept"):
-			reel_hook(delta)
 
-
-func take_hook():
+func on_hook_exit_water():
+	hook.disable()
+	$JerkController.disable()
 	
-	hook.pause(true)
 	hook.set_deferred("position", Vector2.ZERO)
-	hook.set_deferred("linear_velocity", Vector2.ZERO)
 	hook_held = true
+	
+
+
+func on_hook_enter_water():
+	$JerkController.enable()
 
 
 func cast_hook():
-	var direction = Vector2.UP.rotated(deg_to_rad(cast_angle))
-	hook.pause(false)
-	hook.call_deferred("apply_central_impulse", direction * cast_strength)
+	hook.enable()
+	hook.cast(cast_strength, cast_angle)
 	hook_held = false
-
-
-func reel_hook(delta):
-	var direction = (global_position - hook.global_position).normalized()		
-	hook.call_deferred("apply_central_impulse", direction * reel_strength)
-	
