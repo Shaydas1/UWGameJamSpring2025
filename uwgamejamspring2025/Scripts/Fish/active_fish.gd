7b254bd69de6b6b49806c69a050e636ae06272ea
Shaydas1 <43556419+Shaydas1@users.noreply.github.com>
2025-06-07 14:08:28 -0400

    Transitions between flee and wander for fish are working

--- uwgamejamspring2025/Scripts/Fish/active_fish.gd
+++ uwgamejamspring2025/Scripts/Fish/active_fish.gd
@@ -13,6 +13,7 @@ func _process(delta: float) -> void:
 	update_state(delta)
 
 func update_state(delta: float) -> void:
+	#print(current_state)
 	next_state = current_state.update()
 	
 	if next_state != current_state:
